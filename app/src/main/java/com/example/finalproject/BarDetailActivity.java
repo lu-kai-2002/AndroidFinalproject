cdb2f29a3bc8bc5325aaeff2c0eb5181875837e5
wyjirritable <u3641104@connect.hku.hk>
2025-04-03 20:47:16 +0800

    version2

--- app/src/main/java/com/example/finalproject/BarDetailActivity.java
+++ app/src/main/java/com/example/finalproject/BarDetailActivity.java
@@ -19,6 +19,8 @@ public class BarDetailActivity extends AppCompatActivity {
     private RatingBar userRatingBar;
     private TextView tvRatingValue;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
